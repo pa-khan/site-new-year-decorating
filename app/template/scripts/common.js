601c7a9c48e3fc8bca536473981bdcf2be6a4d99
e5ash <e5ash.bro@gmail.com>
2017-10-30 18:55:45 +0300

    Desktop 100%

--- app/template/scripts/common.js
+++ app/template/scripts/common.js
@@ -3,4 +3,14 @@ $(document).ready(function($) {
 	/* Phone mask */
 	$('.input_phone').mask('+7 (999) 999-99-99');
 	
+	$('.reviews__list').slick();
+
+	$(".panel__nav a").click(function (event) {
+		var id  = $(this).attr('href');
+		if(id.charAt(0) == '#'){
+			event.preventDefault();
+			var top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1000);
+		}
+	});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,10 @@ gulp.task('browser-sync', function() {
 gulp.task('scripts', function() {
 	return gulp.src([ 
 		'app/template/libs/jquery/dist/jquery.js',
-		'app/template/libs/jquery.maskedinput/dist/jquery.maskedinput.min.js'
+		'app/template/libs/jquery.maskedinput/dist/jquery.maskedinput.min.js',
+		'app/template/libs/fancybox/dist/jquery.fancybox.js',
+		'app/template/libs/slick-carousel/slick/slick.js'
+
 		])
 		.pipe(concat('libs.js')) 
 		.pipe(gulp.dest('app/template/scripts')); 
